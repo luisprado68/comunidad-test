dc5c7dda20ca788dc17a0af228f371f56a717d26
Luis Prado <lprado@broobe.com>
2024-01-14 17:20:35 -0300

    fix

--- app/Services/TwichService.php
+++ app/Services/TwichService.php
@@ -308,7 +308,7 @@ final class TwichService
                         $current = Carbon::now();
                         $minute = $current->format('i');
 
-                        if ($minute >= env('CHATTERS_MAX_MINUTE') || $minute <= env('CHATTERS_MAX_MINUTE_2')) {
+                        if ($minute >= env('CHATTERS_MAX_MINUTE') && $minute <= env('CHATTERS_MAX_MINUTE_2')) {
                             $score = $user_chat->score;
                             Log::debug('score---------------------');
                             Log::debug($score);
