07f276badcfd447df85bcebaedcf56acbf719420
PradoMarquina <lprado@broobe.com>
2024-01-17 14:43:32 -0300

    fix migration score

--- database/migrations/2023_11_07_235059_create_score_table.php
+++ database/migrations/2023_11_07_235059_create_score_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
             $table->integer('points_day')->nullable();
             $table->integer('points_week')->nullable();
             $table->integer('neo_coins')->nullable();
-            $table->json('users_data')->nullable();
-            $table->integer('count_users')->nullable();
+            $table->json('streamer_supported')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
